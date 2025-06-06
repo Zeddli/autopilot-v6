11bff718e4bd54ea3c23ba9043e328bdce178ebd
billsedison <billsedison@163.com>
2025-06-07 00:38:34 +0800

    Fix config module and docker compose

--- src/kafka/kafka.module.ts
+++ src/kafka/kafka.module.ts
@@ -1,22 +1,15 @@
+// src/kafka/kafka.module.ts
 import { Module, forwardRef } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { KafkaService } from './kafka.service';
 import { KafkaController } from './controllers/kafka.controller';
 import { MessageConsumer } from './consumers/message.consumer';
 import { AutopilotProducer } from './producers/autopilot.producer';
 import { AutopilotConsumer } from './consumers/autopilot.consumer';
-import configuration from '../config/configuration';
-import { validationSchema } from '../config/validation';
+import { ConfigModule } from '@nestjs/config';
 import { AutopilotModule } from '../autopilot/autopilot.module';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-      validationSchema,
-    }),
-    forwardRef(() => AutopilotModule),
-  ],
+  imports: [ConfigModule, forwardRef(() => AutopilotModule)],
   controllers: [KafkaController],
   providers: [
     KafkaService,
