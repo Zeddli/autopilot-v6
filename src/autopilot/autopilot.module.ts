c7cd7c49c3e8d7cf7960fc3369893cbdda33b4b9
Zeddli <ruj8320@gmail.com>
2025-06-17 21:08:14 +0800

    Autopilot service module configuration

--- src/autopilot/autopilot.module.ts
+++ src/autopilot/autopilot.module.ts
@@ -1,10 +1,21 @@
 import { Module, forwardRef } from '@nestjs/common';
 import { AutopilotService } from './services/autopilot.service';
+import { ScheduleAdjustmentService } from './services/schedule-adjustment.service';
+import { ChallengeUpdateHandler } from './handlers/challenge-update.handler';
 import { KafkaModule } from '../kafka/kafka.module';
+import { SchedulerModule } from '../scheduler/scheduler.module';
 
 @Module({
-  imports: [forwardRef(() => KafkaModule)],
-  providers: [AutopilotService],
-  exports: [AutopilotService],
+  imports: [forwardRef(() => KafkaModule), SchedulerModule],
+  providers: [
+    AutopilotService,
+    ScheduleAdjustmentService,
+    ChallengeUpdateHandler,
+  ],
+  exports: [
+    AutopilotService,
+    ScheduleAdjustmentService,
+    ChallengeUpdateHandler,
+  ],
 })
 export class AutopilotModule {}
