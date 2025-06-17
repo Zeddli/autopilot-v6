dfc770b1643e8189e729c98a21c97f2ee3e150c0
Zeddli <ruj8320@gmail.com>
2025-06-17 21:00:08 +0800

    bootstrap and entry point

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppConfigModule } from './config/config.module';
 import { KafkaModule } from './kafka/kafka.module';
 import { AutopilotModule } from './autopilot/autopilot.module';
 import { HealthModule } from './health/health.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { RecoveryModule } from './recovery/recovery.module';
 
 @Module({
-  imports: [AppConfigModule, KafkaModule, AutopilotModule, HealthModule],
+  imports: [
+    ScheduleModule.forRoot(),
+    AppConfigModule,
+    KafkaModule,
+    SchedulerModule,
+    RecoveryModule,
+    AutopilotModule,
+    HealthModule,
+  ],
 })
 export class AppModule {}
