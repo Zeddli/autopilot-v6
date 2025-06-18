1d95e54862a1c6478bfa2646fc585e489e2390d1
Zeddli <ruj8320@gmail.com>
2025-06-18 13:33:10 +0800

    fix import module

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
 import { SchedulerService } from './services/scheduler.service';
 import { KafkaModule } from '../kafka/kafka.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), KafkaModule],
+  imports: [ScheduleModule.forRoot(), forwardRef(() => KafkaModule)],
   providers: [SchedulerService],
   exports: [SchedulerService],
 })
