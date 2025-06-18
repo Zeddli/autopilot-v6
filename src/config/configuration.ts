92a45debcbfae9672f12733a34ebbe19950e2682
Zeddli <ruj8320@gmail.com>
2025-06-18 08:26:06 +0800

    config export

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,11 @@
 import appConfig from './sections/app.config';
 import kafkaConfig from './sections/kafka.config';
+import schedulerConfig from './sections/scheduler.config';
+import recoveryConfig from './sections/recovery.config';
 
 export default () => ({
   app: appConfig(),
   kafka: kafkaConfig(),
+  scheduler: schedulerConfig(),
+  recovery: recoveryConfig(),
 });
